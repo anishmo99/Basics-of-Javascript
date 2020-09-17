0c16ba03596f05e53ac45ff5bb8d6daadc6ae67f
anishmo99 <ani10sh@gmail.com>
2020-09-17 17:00:05 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log(7 === '7') // false
+console.log(7 == 7) // true
+
+console.log(7 != 7) //false
+console.log(7 != '7') //false
+console.log(7 !== 7) //false
+console.log(7 !== '7') //true
